1d1448c4cbef36c3eaf00a7a34a8ffc4026e95ce
lolitee <62479501+lolitee@users.noreply.github.com>
2021-07-12 17:41:54 +0200

    almost done

---
+++ scripts/latest.ps1
@@ -0,0 +1,31 @@
+
+$repo = "lolitee/adobe-discord-rpc"
+$name = "discord rpc"
+
+$releases = "https://api.github.com/repos/$repo/releases"
+
+Write-Host Getting latest version
+$tag = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].tag_name
+$download = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].assets[0].browser_download_url
+$body = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].body
+
+Write-Host $download
+Write-Host $body -ForegroundColor green
+
+$zip = "$name-$tag.zip"
+$dir = "$name-$tag"
+
+Write-Host Downloading latest version
+Invoke-WebRequest $download -Out $zip
+
+Write-Host Extracting files
+Expand-Archive $zip -Force
+
+Remove-Item $name -Recurse -Force -ErrorAction SilentlyContinue 
+
+Move-Item $dir\$name -Destination $name -Force
+
+Remove-Item $zip -Force
+Remove-Item $dir -Recurse -Force
+
+PAUSE
