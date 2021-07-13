3cd2eec62735efc5b70886ad9608702116bd1ad6
Tee <62479501+lolitee@users.noreply.github.com>
2021-07-13 16:48:03 +0200

    Update latest.ps1

--- scripts/latest.ps1
+++ scripts/latest.ps1
@@ -30,6 +30,6 @@ Remove-Item $dir -Recurse -Force
 
 Write-Host "Make sure to restart your Adobe app, if you've updated it through the panel!"
 
-Remove-Item .\\latest.ps1
+Remove-Item .\latest.ps1
 
 PAUSE
