451e4ce54e0be8afe8d1ec9ca0efd071fe6884fb
Tee <62479501+lolitee@users.noreply.github.com>
2021-07-14 11:39:35 +0200

    Update latest.ps1

--- scripts/latest.ps1
+++ scripts/latest.ps1
@@ -10,8 +10,8 @@ $download = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].assets[0].browse
 $body = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].body
 
 If (Test-Path $name){
-    Remove-Item $name -Recurse -Force
-    Write-Host Removing $name
+    Remove-Item $name -Recurse -Force -Ignore
+    Write-Host Removing [$name]
 }
 
 Write-Host $download
