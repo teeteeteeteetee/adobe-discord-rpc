8c3f889796b7b8878f7d86b48f272c38b1c7700c
Tee <sadteeeeeeee@gmail.com>
2023-02-06 17:55:10 +0100

    ye

--- src/rpc_client-src/index.js
+++ src/rpc_client-src/index.js
@@ -4,39 +4,59 @@
 "use strict";
 
 import RichPresence from "./rpc";
-//import { getState, getSmallImageText, getSmallImageKey, getLargeImageText, getPartyMax, getPartySize, getHost } from "./host"
+import isEqual from "lodash/isEqual"
 
 const csInterface = new CSInterface();
 const client = require("./client.js")[csInterface.getApplicationID()];
 
-let interval = 1500;
-let state, details, largeImageText, smallImageKey, smallImageText, partySize, partyMax
+let interval = 1000;
+let props = {
+    state: undefined,
+    details: undefined,
+    startTimestamp: new Date(),
+    largeImageKey: 'logo',
+    largeImageText: undefined,
+    smallImageKey: undefined,
+    smallImageText: undefined,
+    partySize: 0,
+    partyMax: 0,
+}
+let activity = {}
 
 // do not initialize if its ran through dynamic link (after effects)
 if (csInterface.getApplicationID() === "AEFT") {
+    let isDynamicLink = false
     csInterface.evalScript("app.activeViewer", (x) => {
-        if (x === "null") return;
+        if (x === "null")
+            isDynamicLink = true
     });
+    if (isDynamicLink)
+        throw new Error("Started as dynamic link");
 }
 
 const rpc = new RichPresence(client);
+rpc.login()
+.then(() => main())
+.catch(console.error)
+function main() {
+    try {
+        csInterface.evalScript('state()', x => props.state = x);
+        csInterface.evalScript('details()', x => props.details = x);
+        csInterface.evalScript('smallImageKey()', x => props.smallImageKey = x);
+        csInterface.evalScript('smallImageText()', x => props.smallImageText = x);
+        csInterface.evalScript('largeImageText()', x => props.largeImageText = x);
+        csInterface.evalScript('partySize()', x => props.partySize = parseInt(x));
+        csInterface.evalScript('partyMax()', x => props.partyMax = parseInt(x));
 
-rpc.create().then(rpc.login());
+        if (!isEqual(activity, props)) {
+            rpc.setActivity(props)
+            activity = props
+            console.log(props)
+            console.log("not equal")
+        }
 
-setInterval(() => {
-    csInterface.evalScript('state()', x => state = x);
-    csInterface.evalScript('details()', x => details = x);
-
-    rpc.setActivity({
-        state: state,
-        details: details,
-        startTimestamp: 0,
-        largeImageKey: 'logo',
-        largeImageText: largeImageText,
-        smallImageKey: smallImageKey,
-        smallImageText: smallImageText,
-        partySize: partySize,
-        partyMax: partyMax,
-    })
-
-}, interval);
+    } catch (err) {
+        console.error(err);
+    }
+    setTimeout(main, interval);
+}
--- src/rpc_client-src/rpc.js
+++ src/rpc_client-src/rpc.js
@@ -1,38 +1,41 @@
 /**
  * @author Tee
  */
+"use strict";
 
-import { Client, register } from 'discord-rpc'
+import { Client } from 'discord-rpc'
 export default class RichPresence {
 
     client;
     clientId;
 
-    constructor(client){
+    constructor(client) {
         this.clientId = client.id;
     }
-    async create(){
-        this.client = new Client({ transport: 'ipc'})
-        this.client.on('ready', () => {
-            this.setActivity({
-                details: undefined,
-                state: undefined
+    login() {
+        return new Promise((resolve) => {
+            this.client = new Client({ transport: 'ipc' })
+            this.client.on('ready', () => {
+                console.log(`Rich Presence ready! ${this.client.user.username + "#" + this.client.user.discriminator}`);
+                this.setActivity({
+                    details: undefined,
+                    state: undefined
+                })
+                resolve()
             })
+    
+            this.client
+                .login({ clientId: this.clientId })
+                .catch(console.error)
         })
     }
 
-    async login(){
-        await this.client
-        .login({ clientId: this.clientId })
-        .catch(console.error)
-    }
-
-    async destroy(){
-        await this.client.destroy();
+    async destroy() {
+        this.client.destroy();
         delete this.client;
     }
 
-    setActivity(props){
+    setActivity(props) {
         console.log(props)
         this.client.setActivity({
             details: props.details,
@@ -47,7 +50,7 @@ export default class RichPresence {
         })
     }
 
-    getUser(){
+    getUser() {
         return this.client.user;
     }
 }
