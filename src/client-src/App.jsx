060f586da7ef93b00dbe143f7289690f935093d7
Tee <sadteeeeeeee@gmail.com>
2023-02-11 00:56:58 +0100

    renaming

--- src/client-src/App.jsx
+++ src/client-src/App.jsx
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import Navigator from './components/Navigator';
-import Visualizer from './page/Visualizer';
+import Preview from './page/Preview';
 import Config from './page/Config';
 import Debug from './page/Debug';
 export default function App(props) {
 
     const [state, setState] = useState("");
 
-    useEffect(() => setState("Visualizer"))
+    useEffect(() => setState("Preview"))
 
     return (
         <div className='h-screen w-full'>
             <Navigator setState={setState} />
-            {state === "Visualizer" && <Visualizer />}
+            {state === "Preview" && <Preview />}
             {state === "Config" && <Config />}
             {state === "Debug" && <Debug />}
         </div>
--- src/client-src/components/Navigator.jsx
+++ src/client-src/components/Navigator.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import NavItem from "./NavItem";
 import { FaDiscord, FaCog, FaGithub } from 'react-icons/fa'
 import { VscDebugConsole } from 'react-icons/vsc'
@@ -7,7 +7,7 @@ export default function Navigator({setState}) {
     return (
         <nav className="flex items-center justify-between flex-wrap bg-secondary p-6">
             <div className="w-full flex items-center divide-x hover:bg-blend-lighten">
-                <NavItem onClick={() => setState("Visualizer")} href={"#"} text={"Visualizer"} icon={<FaDiscord className="text-gray-200 m-auto" />} />
+                <NavItem onClick={() => setState("Preview")} href={"#"} text={"Preview"} icon={<FaDiscord className="text-gray-200 m-auto" />} />
                 <NavItem onClick={() => setState("Config")} href={"#"} text={"Config"} icon={<FaCog className="text-gray-200 m-auto" />} />
                 <NavItem onClick={() => setState("Debug")} href={"#"} text={"Debug"} icon={<VscDebugConsole className="text-gray-200 m-auto" />} />
                 <NavItem href={"#"} text={"Repository"} icon={<FaGithub className="text-gray-200 m-auto" />} />
